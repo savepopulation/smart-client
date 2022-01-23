f930e9d98dc163f4627270435ba3bf039f5d3d4f
tyln <taylan.sbrcn@gmail.com>
2022-01-23 11:47:45 +0300

    configuration files added

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,15 @@
+/*
+ * Modules
+ * Contains the modules that is used are the project
+ */
+object Modules {
+    /*
+     * Application module
+     */
+    const val app = ":app"
+
+    /*
+     * Gadget Library module
+     */
+    const val lib = ":lib"
+}
---
+++ buildSrc/src/main/java/Paths.kt
@@ -0,0 +1,14 @@
+/*
+ * Contains the path variables that used in project
+ */
+object Paths {
+    const val gradleClasspath = "com.android.tools.build:gradle:${Versions.gradleVersion}"
+    const val kotlinGradlePluginClasspath =
+        "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}"
+    const val googleServicesClasspath =
+        "com.google.gms:google-services:${Versions.googleServicesPluginVersion}"
+    const val firebaseCrashlyticsPath =
+        "com.google.firebase:firebase-crashlytics-gradle:${Versions.firebaseCrashlyticsPluginVersion}"
+    const val firebasePerformancefClasspath =
+        "com.google.firebase:perf-plugin:${Versions.firebasePerformancePluginVersion}"
+}

9d32648b0451bfd2bb0f17114f03754f3536aac1
tyln <taylan.sbrcn@gmail.com>
2022-01-24 12:36:58 +0300

    local data source added to setup

--- lib/src/main/java/com/raqun/smartclient/lib/SmartClient.kt
+++ lib/src/main/java/com/raqun/smartclient/lib/SmartClient.kt
@@ -2,6 +2,8 @@ package com.raqun.smartclient.lib
 
 import android.annotation.SuppressLint
 import android.content.Context
+import com.raqun.smartclient.lib.data.DefaultLocalDataSource
+import com.raqun.smartclient.lib.data.LocalDataSource
 import java.util.UUID
 
 typealias ClientIdGenerator = () -> String
@@ -9,17 +11,18 @@ typealias ClientIdGenerator = () -> String
 @SuppressLint("StaticFieldLeak")
 object SmartClient {
 
-    private lateinit var context: Context
-
     private var clientIdGenerator: ClientIdGenerator = {
         UUID.randomUUID().toString()
     }
 
+    private lateinit var localDataSource: LocalDataSource
+
     fun setup(
         context: Context,
+        localDataSource: LocalDataSource? = null,
         clientIdGenerator: ClientIdGenerator? = null
     ) {
-        this.context = context.applicationContext
+        this.localDataSource = localDataSource ?: DefaultLocalDataSource(context.applicationContext)
         clientIdGenerator?.let {
             this.clientIdGenerator = it
         }

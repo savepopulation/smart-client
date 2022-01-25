0bc059278c9e1e0be46863e313b2fa0bbbd2e318
tyln <taylan.sbrcn@gmail.com>
2022-01-25 13:01:19 +0300

    session id added

--- lib/src/main/java/com/raqun/smartclient/lib/SmartClient.kt
+++ lib/src/main/java/com/raqun/smartclient/lib/SmartClient.kt
@@ -7,21 +7,32 @@ import com.raqun.smartclient.lib.data.LocalDataSource
 import java.util.UUID
 
 typealias ClientIdGenerator = () -> String
+typealias SessionIdGenerator = () -> String
 
 @SuppressLint("StaticFieldLeak")
 object SmartClient {
     private lateinit var localDataSource: LocalDataSource
     private lateinit var _clientId: String
+    private lateinit var _sessionId: String
 
     val clientId: String
         get() = _clientId
 
+    val sessionId: String
+        get() = _sessionId
+
     fun setup(
         context: Context,
         localDataSource: LocalDataSource? = null,
-        clientIdGenerator: ClientIdGenerator = { UUID.randomUUID().toString() }
+        clientIdGenerator: ClientIdGenerator = { UUID.randomUUID().toString() },
+        sessionIdGenerator: SessionIdGenerator = { UUID.randomUUID().toString() }
     ) {
         this.localDataSource = localDataSource ?: DefaultLocalDataSource(context.applicationContext)
-        _clientId = this.localDataSource.getClientId() ?: clientIdGenerator.invoke()
+        _clientId = this.localDataSource.getClientId() ?: kotlin.run {
+            val clientId = clientIdGenerator.invoke()
+            this.localDataSource.saveClientId(clientId)
+            clientId
+        }
+        _sessionId = sessionIdGenerator.invoke()
     }
 }
